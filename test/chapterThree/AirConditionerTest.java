9724dde65ecac48c95f43620fb23a445076e16df
Precious <preciousonome002@gmail.com>
2024-03-08 13:04:11 +0100

    project

--- test/chapterThree/AirConditionerTest.java
+++ test/chapterThree/AirConditionerTest.java
@@ -1,8 +1,9 @@
 package chapterThree;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.*;
 
 
 public class AirConditionerTest {
