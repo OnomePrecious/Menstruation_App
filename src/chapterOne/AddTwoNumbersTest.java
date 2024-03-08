14c0db1e52fb7a693c86aecf2dd11860e5dc8e08
Precious <preciousonome002@gmail.com>
2024-03-08 14:46:10 +0100

    javaProjects

--- src/chapterOne/AddTwoNumbersTest.java
+++ src/chapterOne/AddTwoNumbersTest.java
@@ -1,15 +1,15 @@
-package chapterOne;
-
-import chapterOne.AddTwoNumbers;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class AddTwoNumbersTest {
-    @Test
-    public void TestThat_FunctionCanAddTwoNumbers(){
-        AddTwoNumbers add = new AddTwoNumbers();
-
-        int result = AddTwoNumbers.addition(10, 20);
-        assertEquals(30, result);
-    }
-}
+//package chapterOne;
+//
+//import chapterOne.AddTwoNumbers;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class AddTwoNumbersTest {
+//    @Test
+//    public void TestThat_FunctionCanAddTwoNumbers(){
+//        AddTwoNumbers add = new AddTwoNumbers();
+//
+//        int result = AddTwoNumbers.addition(10, 20);
+//        assertEquals(30, result);
+//    }
+//}
