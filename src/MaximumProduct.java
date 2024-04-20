cba1291a8a828e2d65654293098cfe316c58e6cb
Precious <preciousonome002@gmail.com>
2024-04-20 16:37:43 +0100

    project

---
+++ src/MaximumProduct.java
@@ -0,0 +1,28 @@
+public class MaximumProduct {
+
+    public static int maxiNumberInArray(int... numbers) {
+        int maximumNumber = 0;
+        if(numbers.length == 2) {
+            return numbers[0] * numbers[1];
+        }
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+
+                if (numbers[i] * numbers[j] > maximumNumber) {
+                    maximumNumber = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return maximumNumber;
+    }
+
+
+}
+
+
+
+
+
+
+
+
---
+++ test/MaximumProductTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumProductTest {
+
+    @Test
+    public void test_maxiNumberInAnArray() {
+        int[] numbers = { 1, -5};
+       int result = -5;
+       assertEquals(result, MaximumProduct.maxiNumberInArray(numbers));
+
+    }
+}
+
