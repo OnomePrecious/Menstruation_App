6b81556942777f8d5aa4fd660589644fa5085ae3
Precious <preciousonome002@gmail.com>
2024-04-20 16:39:31 +0100

    project

---
+++ src/SortArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static int[] greaterProduct(int... numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                if (numbers[i] == 0) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+        }
+          return  numbers;
+        }
+
+
+    public static void main(String[] args) {
+        int[] numbers = { 0, 0, 8, 9, 2};
+        System.out.println(Arrays.toString(greaterProduct(numbers)));
+    }
+}
+
