c15deb349268335897010311371339b3669ad722
Precious <preciousonome002@gmail.com>
2024-05-18 16:50:30 +0100

    project

--- src/chapter17/FunctionalInterface/assignment/DoubleUnaryOperatorSample.java
+++ src/chapter17/FunctionalInterface/assignment/DoubleUnaryOperatorSample.java
@@ -1,4 +1,13 @@
 package chapter17.FunctionalInterface.assignment;
 
+import java.util.function.DoubleUnaryOperator;
+
 public class DoubleUnaryOperatorSample {
+    public static void main(String[] args) {
+        DoubleUnaryOperator doubleUnaryOperator = (number) -> 2 * number;
+        double number = doubleUnaryOperator.applyAsDouble(76.89);
+        System.out.println(number);
+
+
+    }
 }
