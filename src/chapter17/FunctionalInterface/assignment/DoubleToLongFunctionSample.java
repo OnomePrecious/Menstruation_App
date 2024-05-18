d4b49363caa52469b7628a2fff952022c83931eb
Precious <preciousonome002@gmail.com>
2024-05-18 16:45:01 +0100

    project

--- src/chapter17/FunctionalInterface/assignment/DoubleToLongFunctionSample.java
+++ src/chapter17/FunctionalInterface/assignment/DoubleToLongFunctionSample.java
@@ -1,7 +1,12 @@
 package chapter17.FunctionalInterface.assignment;
 
+import java.util.function.DoubleToLongFunction;
+
 public class DoubleToLongFunctionSample {
     public static void main(String[] args) {
+        DoubleToLongFunction doubleToLongFunction = (doubleNumber) -> Double.valueOf(doubleNumber).longValue();
+        long number = doubleToLongFunction.applyAsLong(65.43);
+        System.out.println(number);
 
     }
 }
