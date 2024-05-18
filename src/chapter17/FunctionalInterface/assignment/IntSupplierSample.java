bd246f8d11a0562256a4c7c5be617e86598182fa
Precious <preciousonome002@gmail.com>
2024-05-18 17:16:34 +0100

    project

---
+++ src/chapter17/FunctionalInterface/assignment/IntSupplierSample.java
@@ -0,0 +1,12 @@
+package chapter17.FunctionalInterface.assignment;
+
+import java.util.Random;
+import java.util.function.IntSupplier;
+
+public class IntSupplierSample {
+    public static void main(String[] args) {
+        IntSupplier intSupplier = () -> new Random().nextInt();
+        System.out.println(intSupplier.getAsInt());
+
+    }
+}
