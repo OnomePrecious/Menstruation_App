077f16d47aee7b5a4ad33d90dc64e8d00e4c3677
Precious <preciousonome002@gmail.com>
2024-05-20 13:31:46 +0100

    project

--- src/chapter17/FunctionalInterface/consumer/ConsumerSample.java
+++ src/chapter17/FunctionalInterface/consumer/ConsumerSample.java
@@ -5,10 +5,7 @@ import java.util.function.Consumer;
 
 public class ConsumerSample {
     public static void main(String[] args) {
-        Consumer<String> consumer = (s)->{
-            System.out.println(s);
-
-        };
+        Consumer<String> consumer = System.out::println;
         List<String> names = List.of("John", "Joe", "Johnny");
         names.forEach(consumer);
     }
